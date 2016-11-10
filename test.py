ee6966a4ecf798b2612384172acf971bd2017479
kfischer1 <kfischer2@babson.edu>
2016-11-10 13:19:31 -0500

    commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import json
-from pprint import pprint
-
-url = "https://maps.googleapis.com/maps/api/geocode/json?address=Prudential%20Tower"
-f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
-response_data = json.loads(response_text)
-pprint(response_data)
