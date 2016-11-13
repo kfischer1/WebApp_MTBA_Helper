85d05beb4d28bbcdba286a160fdab7734f0bebe7
Hunter Schilb <hschilb1@babson.edu>
2016-11-13 14:48:22 -0500

    Update webapp.py
    Updated web app - flask

--- webapp.py
+++ webapp.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from MBTA_helper1 import alert_user
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        location = request.form['location']
+        stop, d = alert_user(location)
+        if stop:
+            return render_template('result.htm', stop= stop, distance = d)
     return render_template("home.htm")
+
 
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+
 
 
 
